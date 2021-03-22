18db6eb47e78a6372a55d72a7a8cebbed028b069
peter5600 <peter.colquhoun3@gmail.com>
2021-03-22 13:45:41 +0000

    Added presentation

--- todolist/src/test/java/com/qa/todolist/controller/TaskControllerIntegrationTest.java
+++ todolist/src/test/java/com/qa/todolist/controller/TaskControllerIntegrationTest.java
@@ -3,10 +3,7 @@ package com.qa.todolist.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> dev
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
